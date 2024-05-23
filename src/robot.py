ab6816492890b7a7171f7273d9a56c701388481d
BuildTools <unconfigured@null.spigotmc.org>
2024-05-23 15:03:36 -0400

    redo all code and uses new lib(for now until YAGSL python comes out)

---
+++ src/robot.py
@@ -0,0 +1,25 @@
+# You must either:
+#   1) Hardlink swervepy into the example_robot directory OR
+#   2) Move the example code to a different project and copy swervepy into that project
+
+from typing import Optional
+import swervepy
+import commands2
+
+from container import RobotContainer
+
+
+class Robot(commands2.TimedCommandRobot):
+    def robotInit(self):
+        self.container = RobotContainer()
+        self.scheduler = commands2.CommandScheduler.getInstance()
+        self.autonomous_command: Optional[commands2.Command] = None
+
+    def autonomousInit(self) -> None:
+        self.autonomous_command = self.container.get_autonomous_command()
+        if self.autonomous_command:
+            self.autonomous_command.schedule()
+
+    def teleopInit(self) -> None:
+        if self.autonomous_command:
+            self.autonomous_command.cancel()
